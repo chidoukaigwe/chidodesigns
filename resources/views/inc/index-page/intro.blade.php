3fc6a78eee8196f33e95b0bc93afee6c4f923d54
Chido Ukaigwe <chidodesigns@gmail.com>
2020-09-11 07:23:28 +0100

    updated index page content

--- resources/views/inc/index-page/intro.blade.php
+++ resources/views/inc/index-page/intro.blade.php
@@ -5,10 +5,10 @@
             <div class="jumbotron p-3 p-md-5 rounded custom-jumbotron">
 
                 <img src="/images/main/Chido.jpg" alt=""  style="border-radius:15%" class="mb-4">
-                <h2 class="mb-4">Chido Ukaigwe - <small>Full Stack PHP Dev</small></h2>
+                <h2 class="mb-4">Chido Ukaigwe: <small>Full Stack PHP Dev</small></h2>
                 <h3 class="mb-4">Lead Developer<small><a href="https://www.bplmarketing.com/" target="_blank"> @bplmarketing</a></small></h2>
-                <p class="mb-2">I am a highly motivated, self-starting web developer; building web and REST applications in PHP frameworks such as Laravel and Symfony.</p>
-                <p class="mb-2">Wordpress plugin and theme development; with a focus on developing decoupled content management systems with API interfaces.</p>
+                <p class="mb-2">I am a highly motivated, self-starting full stack developer; building web, SPA's and REST applications in PHP frameworks such as Laravel and Symfony.</p>
+                <p class="mb-2">Along with Wordpress plugin and theme development; with a focus on developing decoupled content management systems with API interfaces.</p>
                 <p class="mt-4 mb-4">
                     <a href="/tech-skills" class="btn btn-lg btn-secondary" role="button">Tech Skills</a>
                     <a href="/work-experience" class="btn btn-lg btn-secondary" role="button">Work Experience</a>
@@ -26,7 +26,7 @@
                 <p class="custom-jumbotron-lead-text text-center">Current Project - Completion Date: Sep 2020</p>
                 <ul>
                     <li>
-                        Developing a custom wordpress (theme & plugins) ecommerce multisite for an international dog accessories company. <br>
+                        Developing a custom wordpress (theme & plugins) e-commerce multisite for an international dog accessories company. <br>
                         Client: <a href="https://www.companyofanimals.co.uk/" target="_blank">Company Of Animals</a> <br>
                         Build: <a href="http://coa-static.bpl-staging.com/index.html" target="_blank">Company Of Animals Static Site</a>
 
@@ -39,6 +39,9 @@
                     </li>
                     <li>
                         Hybrid application architecture built into the backend. Combining the native Wordpress ecosystem with its RESTful services.
+                    </li>
+                    <li>
+                        Custom e-commerce features, using the popular wordpress woocommerce plugin, that enable the sale of goods on the platform.
                     </li>
                 </ul>
             </div>
