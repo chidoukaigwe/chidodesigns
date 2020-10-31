10fc9b11bcced9f9d140f250ca0e3103aa0ab1b3
Chido Ukaigwe <chidodesigns@gmail.com>
2020-10-31 19:50:46 +0000

    updated the intro section of the index page

--- storage/framework/views/bca21af79dacfea6a182c20a2833973f88a5f616.php
+++ storage/framework/views/bca21af79dacfea6a182c20a2833973f88a5f616.php
@@ -6,21 +6,34 @@
 
             <img src="/images/main/Chido.jpg" alt="" style="border-radius:15%" class="mb-4">
             <h2 class="mb-4">Chido Ukaigwe: <small>Full Stack PHP Dev</small></h2>
-            <p class="mb-2">I am a highly motivated, self-starting full stack developer; building web, SPA's and REST applications in PHP frameworks such as Laravel and Symfony.</p>
-            <p class="mb-2">I am also a Wordpress plugin and theme developer. With a focus on developing decoupled
-                content management systems that have a javascript focused front end.</p>
+
             <p class="mb-2">
-                I have DevOps commercial experience which combines well, with my full stack
-                development capabilities.
+                I am a highly motivated, self-starting full stack developer; building web, SPA's and REST applications in PHP frameworks such as Laravel and Symfony.
             </p>
+
+            <p class="mb-2">
+                I am also a Wordpress plugin and theme developer. With a focus on developing decoupled
+                content management systems that have a javascript focused front end.
+            </p>
+
+            <p class="mb-2">
+            Javascript is my secondary language, building backend applications with Node.js, whilst using frontend javascript frameworks like Vue.js and Angular.js for consumer facing interfaces.
+            </p>
+
+            <p class="mb-2">
+                I have DevOps commercial experience which combines well, with my full stack development capabilities.
+            </p>
+
             <p class="mb-2">
             My ambition is to build out a balanced skillset and workflow between the two disciplines.
             </p>
+
             <p class="mt-4 mb-4">
                 <a href="/tech-skills" class="btn btn-lg btn-secondary" role="button">Tech Skills</a>
                 <a href="/work-experience" class="btn btn-lg btn-secondary" role="button">Work Experience</a>
                 <a href="/contact" class="btn btn-lg custom-btn" role="button">Hire Me</a>
             </p>
+
         </div>
 
     </div>
@@ -33,19 +46,22 @@
 
             <ul>
                 <li class="mb-2">
-                   <a href="https://companyofanimals.co.uk/" target="_blank">Company Of Animals</a>: Wordpress Multisite for a pet accessories company.
+                   <a href="https://companyofanimals.co.uk/" target="_blank">Company Of Animals</a>: Custom Wordpress multi-site for a international pet accessories company.
                 </li>
                 <li class="mb-2">
-                   <a href="https://agencyspace.co.uk/" target="_blank">Agency Space</a>: Headless/Decoupled Wordpress website for a creative agency.
+                   <a href="https://agencyspace.co.uk/" target="_blank">Agency Space</a>: Headless - Decoupled custom Wordpress website for a creative agency.
                 </li>
                 <li class="mb-2">
-                   <a href="https://cms.chido-designs.co.uk/login" target="_blank">Chido Designs CMS</a>: A multi user Laravel / Vue.js Single Page Application. A content management system that will eventually power this website.
+                   <a href="https://cms.chido-designs.co.uk/login" target="_blank">Chido Designs CMS</a>: Laravel 7 / Vue.js SPA content management system. Create and publish articles.
                 </li>
                 <li class="mb-2">
-                   <a href="http://social-network.chido-designs.co.uk/" target="_blank">Social Network Sandbox</a>: A multi user Node.js, social network. That allows users to create posts and follow each other.
+                   <a href="http://social-network.chido-designs.co.uk/" target="_blank">Social Network</a>: A multi user Node.js - social network application. That allows users to create posts and follow each other.
                 </li>
                 <li class="mb-2">
-                   <a href="https://contacts.chido-designs.co.uk/register" target="_blank">Contact Management System</a>: A multi user Laravel / Vue.js Single Page Application. This app allows you to create and store contact details.
+                   <a href="https://contacts.chido-designs.co.uk/register" target="_blank">Chido Designs Address Book App</a>: A Laravel - Vue.js - Tailwind CSS single page application. This app allows you to create and store contact details.
+                </li>
+                <li class="mb-2">
+                   <a href="https://hostfront.co.uk/" target="_blank">Hostfront</a>: Headless - Decoupled custom Wordpress website with an Angular 9 frontend. The service offers bespoke Wordpress hosting.
                 </li>
             </ul>
         </div>
